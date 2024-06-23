b1c1416a4dbe0573dea07c8ee32a913d311de98c
Simran Nijjar <simran.nijjar@hotmail.com>
2024-06-23 12:50:38 -0700

    Update navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">My Library</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+      {/*My Books*/}
+      <li class="nav-item">
+          <a class="nav-link" href="#">My Books</a>
+        </li>
+
+        {/*Login*/}
+        <li className="nav-item">
+          <a className="nav-link" aria-current="page" href="#">Login</a>
+        </li>
+
+        {/*Register*/}
+        <li className="nav-item">
+          <a className="nav-link" href="#">Register</a>
+        </li>
+      </ul>
     </div>
+    <form className="d-flex form-inline my-2 my-lg-0">
+      <input className="form-control me-sm-2" type="search" placeholder="Search Books" aria-label="Search"/>
+      <button className="btn btn-outline-light" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
   );
 }
 

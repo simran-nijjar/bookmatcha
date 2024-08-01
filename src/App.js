ba21b596aee12cbee7ab3a907c9767cdca555705
Simran Nijjar <simran.nijjar@hotmail.com>
2024-08-01 16:13:28 -0700

    Make navbar collapse when window size decreases

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min'; // Ensure Bootstrap JS is included
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Route, Routes, Navigate, Link } from 'react-router-dom';
 import { Login } from "./Pages/Login";
@@ -47,22 +48,29 @@ function App() {
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav"></ul>
-          </div>
-          <div className="navbar-nav ml-auto">
+            <ul className="navbar-nav">
+              {isLoggedIn && (
+                <>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/BookRecommendations">Recommendations</Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/UserAccount">My Account</Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/" onClick={handleLogout}>Logout</Link>
+                  </li>
+                </>
+              )}
+            </ul>
             {isLoggedIn && (
-              <>
-              <li className="nav-item">
-                  <a className="nav-link" href="/BookRecommendations">Recommendations</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/UserAccount">My Account</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/" onClick={handleLogout}>Logout</a>
-                </li>
-                <SearchBar query={query} onQueryChange={(event) => handleQueryChange(event, setQuery)} onSearch={(event) => handleSearch(event, query, setResults, setTotalPages, setCurrentPage)} />
-              </>
+              <div className="ms-auto">
+                <SearchBar 
+                  query={query}
+                  onQueryChange={(event) => handleQueryChange(event, setQuery)}
+                  onSearch={(event) => handleSearch(event, query, setResults, setTotalPages, setCurrentPage)} 
+                />
+              </div>
             )}
           </div>
         </div>
@@ -72,7 +80,13 @@ function App() {
         <Route path="/Register" element={<Register onLogin={handleLogin} />} />
         <Route path="/UserAccount" element={<UserAccount onLogout={handleLogout} />} />
         <Route path="/" element={isLoggedIn ? <Navigate to="/HomePage" /> : <LandingPage />} />
-        <Route path="/BookResults" element={<BookResults results={results} currentPage={currentPage} totalPages={totalPages} onNextPage={() => handleNextPage(currentPage, query, setResults, setCurrentPage)} onPrevPage={() => handlePrevPage(currentPage, query, setResults, setCurrentPage)} />} />
+        <Route path="/BookResults" element={<BookResults 
+          results={results} 
+          currentPage={currentPage} 
+          totalPages={totalPages} 
+          onNextPage={() => handleNextPage(currentPage, query, setResults, setCurrentPage)} 
+          onPrevPage={() => handlePrevPage(currentPage, query, setResults, setCurrentPage)} 
+        />} />
         <Route path="/HomePage" element={<HomePage />} />
         <Route path="/BookRecommendations" element={<BookRecommendations />} />
         <Route path="/book/:id" element={<BookDetails />} />

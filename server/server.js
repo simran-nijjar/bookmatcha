e24b8c9e384c86c6d98295141dfb0bf125062df9
Simran Nijjar <simran.nijjar@hotmail.com>
2026-02-16 21:32:33 -0800

    Test prod deployment

--- server/server.js
+++ server/server.js
@@ -28,6 +28,16 @@ const PORT = process.env.PORT;
 
 console.log("PORT ENV VALUE:", process.env.PORT);
 
+process.on('uncaughtException', (err) => {
+  console.error('Uncaught Exception:', err);
+  process.exit(1);
+});
+
+process.on('unhandledRejection', (reason) => {
+  console.error('Unhandled Rejection:', reason);
+  process.exit(1);
+});
+
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });

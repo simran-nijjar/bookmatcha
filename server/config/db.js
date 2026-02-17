330d7d24801eb31a5c5f5943e28ba21bd9f22fb8
Simran Nijjar <simran.nijjar@hotmail.com>
2026-02-16 20:32:47 -0800

    Update SQL env variable names to match prod

--- server/config/db.js
+++ server/config/db.js
@@ -2,10 +2,11 @@ const mysql = require('mysql');
 
 // Connect to database
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT
 });
 
 connection.connect((err) => {
